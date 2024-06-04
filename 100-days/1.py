2efc10d0ad0a543cc156c7d39e65bb80f4dc7b63
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-06-03 19:22:27 -0600

    1

---
+++ 100-days/1.py
@@ -0,0 +1,17 @@
+## Printing, Commenting, Debugging, String Manipulation and Variables
+print("Hello World!")
+
+print("Hello World\nHello Test")
+
+print("Hello " + "Pandi")
+
+#variable names
+first_name = "Pandiarajan"
+address1 = "Av Cuahtemoc"
+# 1address = "Av Cuahtemoc" - invalid
+
+#input from cmd
+name = input("Enter your name ")
+pet = input("enter your pet name ")
+print(len(name))
+print("Your Band Name is " + name + " " + pet)
