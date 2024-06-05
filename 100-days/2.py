531f3e365c9530033ea3f0a0616fc2e207211814
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-06-04 22:38:27 -0600

    operators
    operators

--- 100-days/2.py
+++ 100-days/2.py
@@ -15,7 +15,7 @@ print(3.13 + 4.56)
 print(True)
 print(False)
 
-print("==========Type Casting=============")
+print("========== Type Casting =============")
 
 # Type Casting
 print(str(100)) # "100"
@@ -30,3 +30,20 @@ first_name = "Pandiarajan"
 num_char = len(first_name)
 
 print("Your name has " + str(num_char)+ " character")
+
+#Operators in python
+print("========== Operators =============")
+
+print(3 + 3)
+
+print(7 - 5)
+
+print(7 * 5)
+
+print(200 / 5) #float
+
+print(5 ** 3)
+
+print(204 // 5) #40 -> return the divided by 5 and not remaining
+
+print(204 % 5) #4 -> return remaining after divided by 5 
