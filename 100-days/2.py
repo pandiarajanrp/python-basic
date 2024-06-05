3d50c01ed3d2f6466c40a3d06a957a6a62e5621e
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-06-04 22:51:04 -0600

    fstring

--- 100-days/2.py
+++ 100-days/2.py
@@ -33,6 +33,8 @@ print("Your name has " + str(num_char)+ " character")
 
 #Operators in python
 print("========== Operators =============")
+#Operators Priority
+# PEMDASR - Paranthese, Exponent(of), Mul, Div, Add, Sub, R(Right) from left to right
 
 print(3 + 3)
 
@@ -47,3 +49,15 @@ print(5 ** 3)
 print(204 // 5) #40 -> return the divided by 5 and not remaining
 
 print(204 % 5) #4 -> return remaining after divided by 5 
+
+#floor
+print(round(8 / 3))
+
+#round of
+print(round(8 / 3, 3))
+
+#f-string
+score = 56
+height = 5.6
+isMale = True
+print(f"Your score is {score}, and height is {height}, and you are male {isMale}")
