2a41cf3fef6998d3dd983e2074d5916ffa76621a
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-06-04 22:25:40 -0600

    data types

---
+++ 100-days/2.py
@@ -0,0 +1,32 @@
+#Datatypes
+print("========== Data Types =============")
+#String
+print("Hello" + "World")
+
+#Integer
+print(123 + 456)
+
+print(567_789_764)
+
+#Float
+print(3.13 + 4.56)
+
+#Boolean
+print(True)
+print(False)
+
+print("==========Type Casting=============")
+
+# Type Casting
+print(str(100)) # "100"
+
+print(int("100") + int("120")) #220
+
+print(float("100.5") + float("200.45")) #300.95
+
+print(type("welcome")) #str
+
+first_name = "Pandiarajan"
+num_char = len(first_name)
+
+print("Your name has " + str(num_char)+ " character")
