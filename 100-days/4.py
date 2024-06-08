80b467ccb303fe9d2c43d08b56520e14d58e371b
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-06-07 18:44:19 -0600

    list

--- 100-days/4.py
+++ 100-days/4.py
@@ -2,3 +2,15 @@ import random
 
 rand_number = random.randint(1,100)
 print(rand_number)
+
+#list
+
+states = ["CDMX", "Oxaca", "Yucatan"]
+
+states.append("Merida")
+
+states.extend(["Morelos", "Taxco"])
+
+print(states)
+print(states.pop())
+print(states)
