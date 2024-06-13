55add01706337bce2e3679d14faece29b3a0a026
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-06-12 19:16:48 -0600

    while loop

---
+++ 100-days/6.py
@@ -0,0 +1,26 @@
+#while loop
+
+count = 0
+
+# while count < 10:
+#   count += 1
+#   print(count)
+
+
+#two pointer
+
+list_data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+left_index = 0
+right_index = len(list_data) - 1
+
+sum = 0
+
+while(left_index < right_index):
+  sum += list_data[left_index] + list_data[right_index]
+  left_index += 1
+  right_index -= 1
+  print(f"Left : {left_index}")
+  print(f"Right : {right_index}")
+
+print(sum)
