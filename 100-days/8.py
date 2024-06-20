76fd030c010fdde1d5e2b0cb1ffb00e737affa6c
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-06-19 19:57:44 -0600

    ceasr cipher challenge

--- 100-days/8.py
+++ 100-days/8.py
@@ -1,4 +1,32 @@
 def paint_cal(height, width, coverage):
   return (height * width) / coverage
 
-print(paint_cal(4,5,3))
+# Ceasar Ciber Encryption
+
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#direction = input("Enter 'encode' for encode and 'decode' for decode ")
+text = input(f"Enter text ").lower()
+shift = int(input("Enter shift length "))
+
+text_len = len(text)
+
+def encode():
+  encoded_text = ''
+  for letter in text:
+    idx = alphabets.index(letter)
+    idx_shift = idx + shift
+    new_position = (idx_shift - 26) if idx_shift > 25 else idx_shift
+    encoded_text += alphabets[new_position]
+  return encoded_text
+
+def decode():
+  decoded_text = ''
+  for letter in text:
+    idx = alphabets.index(letter)
+    idx_shift = idx - shift
+    decoded_text += alphabets[idx_shift]
+  return decoded_text
+
+#print(f"encoded {encode()}")
+print(f"decoded {decode()}")
